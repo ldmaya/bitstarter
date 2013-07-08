80cc44fc6d5076dcc6746c6ec4212b17bcba090f
Luis D. Maya <ldmaya@gmail.com>
2013-07-08 02:52:15 +0000

    reads from file

--- web.js
+++ web.js
@@ -3,7 +3,15 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  fs.readFile('index.html', function (err, data) {
+    if (err) throw err;
+    console.log(data);
+    var res = data;
+    if(res.isBuffer) {
+      res = res.toString();
+    }
+    response.send(res);
+  });
 });
 
 var port = process.env.PORT || 5000;
